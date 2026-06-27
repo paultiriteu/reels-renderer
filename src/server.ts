fdfffc577a7eb730001a4bc8fb87247c182b41ee
Paul-Ioan-George Tiriteu <paultiriteu@gmail.com>
2026-06-27 16:59:03 +0300

    Update import path for render module to include .js

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { randomUUID } from "crypto";
 import path from "path";
 import fs from "fs";
 import os from "os";
-import { renderReel } from "./render";
+import { renderReel } from "./render.js";
 
 const app = express();
 app.use(express.json());
